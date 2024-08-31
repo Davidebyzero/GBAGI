c87863a9cef5bb6d7bd5d3bcdc2915979102c13b
andersnm <andersnm@users.noreply.github.com>
2024-08-31 16:05:53 +0200

    romgui fixes for msvc: converted dfm dialog resources to rc source, restored rc source for mainicon resource

---
+++ romgui/addgame.res.h
@@ -0,0 +1,24 @@
+#if !defined addgame_hpp_
+	#define addgame_hpp_
+
+#define IDC_TFORMADDGAME 1001
+
+#define IDC_GROUPBOX1 100
+#define IDC_GROUPBOX2 104
+#define IDC_PANEL5 113
+#define IDC_LABEL1 101
+#define IDC_LABEL2 105
+#define IDC_LABEL3 106
+#define IDC_LABEL4 107
+#define IDC_LABEL5 108
+#define IDC_DROPGAMES 102
+#define IDC_DROPVERSION 109
+#define IDC_PANEL11 114
+#define IDC_TBPATH 103
+#define IDC_TBNAME 110
+#define IDC_TBID 111
+#define IDC_BTNAUTODETECT 112
+#define IDC_BTNOK 115
+#define IDC_BTNCANCEL 116
+
+#endif
