Davidebyzero/GBAGI

commit 41c73137cb54e1c00fc4d4d84d87644a92f05107

romgui for msvc: Support Unicode strings (except where written to the ROM)

romgui/makerom.cpp
@@ -23,20 +23,21 @@
 #include "commands.h"   
 #include "verdef.h"
 #include <conio.h>
+#include <tchar.h>
 /******************************************************************************/
 
 /******************************************************************************/
 GAMEINFO *gi;
 
 DIRENT dirs[4][256];
-const char *dirNames[4] = {"logdir","picdir","viewdir","snddir"};
-const char *resnames[4] = {"logic","picture","view","sound"};
+const TCHAR *dirNames[4] = {_T("logdir"),_T("picdir"),_T("viewdir"),_T("snddir")};
+const TCHAR *resnames[4] = {_T("logic"),_T("picture"),_T("view"),_T("sound")};
 char agiid[IDSIZE]="GBAGI 1.0 '''BRIPRO'''\0";
 
-char fname[1024];
+TCHAR fname[1024];
 char wordbuf[128];
 
-char *inromName,*outromName,*vocabName;
+TCHAR *inromName,*outromName,*vocabName;
 
 BOOL VUSED[16];
 U8 *volData;
@@ -103,18 +104,18 @@ const char *solidGroups[SG_TOTAL][SG_WORDMAX] = {
 	{""}
 };
 /******************************************************************************/
-char strbuf[4096];
+TCHAR strbuf[4096];
 #include <windows.h>
-int ErrorMessage(const char *s, ...)
+int ErrorMessage(const TCHAR *s, ...)
 {
 	va_list argptr;
 	int cnt;
 
 	va_start(argptr, s);
-	cnt = vsprintf(strbuf, s, argptr);
+	cnt = _vstprintf(strbuf, _countof(strbuf), s, argptr);
 	va_end(argptr);
 
-	::MessageBox(NULL,strbuf,"Error",0);
+	::MessageBox(NULL,strbuf,_T("Error"),0);
     exit(1);
 
 	return(cnt);
@@ -177,9 +178,9 @@ BOOL LoadDir(BOOL SINGLE, int num)
 	U32 offs;
 	int totalEnts, i;
 	if(SINGLE) {
-		sprintf(fname,"%s%s%s",gi->path,gi->vID,gi->vID[0]?"dir":"dirs");
-		if((f=fopen(fname,"rb"))==NULL) {
-			ErrorMessage("Unable to open file: %s, for reading!",fname);
+		_stprintf_s(fname,_countof(fname),_T("%s%s%s"),gi->path,gi->vID,gi->vID[0]?_T("dir"):_T("dirs"));
+		if((f=_tfopen(fname,_T("rb")))==NULL) {
+			ErrorMessage(_T("Unable to open file: %s, for reading!"),fname);
 			return FALSE;
 		}
 		fseek(f,num<<1,SEEK_SET);
@@ -191,9 +192,9 @@ BOOL LoadDir(BOOL SINGLE, int num)
 			totalEnts = (fgetw(f)-offs)/3;
 		fseek(f,offs,SEEK_SET);
 	} else {
-		sprintf(fname,"%s%s",gi->path,dirNames[num]);
-		if((f=fopen(fname,"rb"))==NULL) {
-			ErrorMessage("Unable to open file: %s, for reading!",fname);
+		_stprintf_s(fname,_countof(fname),_T("%s%s"),gi->path,dirNames[num]);
+		if((f=_tfopen(fname,_T("rb")))==NULL) {
+			ErrorMessage(_T("Unable to open file: %s, for reading!"),fname);
 			return FALSE;
 		}
 		fseek(f,0,SEEK_END);
@@ -223,8 +224,8 @@ BOOL PrepDirs()
 	volSize=0;
 	for(vn=0;vn<16;vn++) {
     	if(!VUSED[vn]) continue;
-		sprintf(fname,"%s%svol.%d",gi->path,gi->vID,vn);
-		if((f=fopen(fname,"rb"))==NULL) {
+		_stprintf_s(fname,_countof(fname),_T("%s%svol.%d"),gi->path,gi->vID,vn);
+		if((f=_tfopen(fname,_T("rb")))==NULL) {
         	VUSED[vn] = FALSE;
         	continue;
 			//ErrorMessage("Unable to open file: \"%s\", for reading!",fname);
@@ -233,8 +234,8 @@ BOOL PrepDirs()
 		for(t=0;t<4;t++) {
 			for(i=0;i<256;i++) {
 				if(dirs[t][i].vol == vn) {
-					sprintf(fname,"%s%s.%03d",gi->path,resnames[t],i);
-                	if((fr=fopen(fname,"rb"))!=NULL) {
+					_stprintf_s(fname,_countof(fname),_T("%s%s.%03d"),gi->path,resnames[t],i);
+                	if((fr=_tfopen(fname,_T("rb")))!=NULL) {
                         dirs[t][i].vol = 0x7F;
                         fseek(fr,0,SEEK_END);
 						dirs[t][i].length = (U16)ftell(fr);
@@ -277,9 +278,9 @@ BOOL ProcessDirs()
 	for(vn=-1;vn<16;vn++) {
     	if(vn!=-1) {
         	if(!VUSED[vn]) continue;
-			sprintf(fname,"%s%svol.%d",gi->path,gi->vID,vn);
-			if((f=fopen(fname,"rb"))==NULL) {
-				ErrorMessage("Unable to open file: %s, for reading!",fname);
+			_stprintf_s(fname,_countof(fname),_T("%s%svol.%d"),gi->path,gi->vID,vn);
+			if((f=_tfopen(fname,_T("rb")))==NULL) {
+				ErrorMessage(_T("Unable to open file: %s, for reading!"),fname);
 				mFree(fbuf);
 				return FALSE;
 			}
@@ -297,9 +298,9 @@ BOOL ProcessDirs()
 						declen 	= fgetw(f);
 						enclen 	= (gi->version->ver.flags&PACKED_DIRS)?fgetw(f):declen;
                 	} else {
-						sprintf(fname,"%s%s.%03d",gi->path,resnames[t],i);
-						if((f=fopen(fname,"rb"))==NULL) {
-							ErrorMessage("Unable to open file: %s, for reading!",fname);
+						_stprintf_s(fname,_countof(fname),_T("%s%s.%03d"),gi->path,resnames[t],i);
+						if((f=_tfopen(fname,_T("rb")))==NULL) {
+							ErrorMessage(_T("Unable to open file: %s, for reading!"),fname);
 							mFree(fbuf);
 							return FALSE;
 						}          
@@ -353,9 +354,9 @@ BOOL ProcessObject()
     int objCount, i, entSize;
     U8 *p, *iPtr, *u;
 
-	sprintf(fname,"%sobject",gi->path);
-	if((f=fopen(fname,"rb")) == NULL) {
-		ErrorMessage("Unable to open file: \"%s\" for reading!", fname);
+	_stprintf_s(fname,_countof(fname),_T("%sobject"),gi->path);
+	if((f=_tfopen(fname,_T("rb"))) == NULL) {
+		ErrorMessage(_T("Unable to open file: \"%s\" for reading!"), fname);
 		return FALSE;
 	}
 	fseek(f,0,SEEK_END);
@@ -380,7 +381,7 @@ BOOL ProcessObject()
     iPtr		= p+entSize;
     strSize		= l-nameStart;
 	if((objNameData = (char*) malloc(strSize))==NULL) {
-     	ErrorMessage("Unable to allocate %d bytes of memory!", strSize);
+     	ErrorMessage(_T("Unable to allocate %d bytes of memory!"), strSize);
      	return FALSE;
     }
     memcpy(objNameData,iPtr+nameStart, strSize);
@@ -397,18 +398,18 @@ BOOL ProcessObject()
     return TRUE;
 }
 /******************************************************************************/
-U8 *LoadFile(BOOL G_PATH, const char *name, int *len)
+U8 *LoadFile(BOOL G_PATH, const TCHAR *name, int *len)
 {
 	FILE *f;
 	int l;
     U8 *p;
 
 	if(G_PATH)
-    	sprintf(fname,"%s%s",gi->path,name);
+    	_stprintf_s(fname,_countof(fname),_T("%s%s"),gi->path,name);
     else
-    	strcpy(fname,name);
-	if((f=fopen(fname,"rb")) == NULL) {
-		ErrorMessage("Unable to open file: \"%s\" for reading!", fname);
+    	_tcscpy(fname,name);
+	if((f=_tfopen(fname,_T("rb"))) == NULL) {
+		ErrorMessage(_T("Unable to open file: \"%s\" for reading!"), fname);
 		return FALSE;
 	}
 	fseek(f,0,SEEK_END);
@@ -621,7 +622,7 @@ int AlphaSortWords()
     Form1->ListBox2->Items->Clear();
     while(w1->group) {
     	q++;
-    	Form1->ListBox2->Items->Add(AnsiString(w1->string));
+    	Form1->ListBox2->Items->Add(VclString(w1->string));
 		w1++;
     }        */
     w1 = wordset;
@@ -646,7 +647,7 @@ int AlphaSortWords()
     }         /*
     w1 = wordset;
     while(w1->group) {
-    	Form1->ListBox3->Items->Add(AnsiString(w1->string));
+    	Form1->ListBox3->Items->Add(VclString(w1->string));
 		w1++;
     }   */
 
@@ -661,7 +662,7 @@ BOOL ProcessWords()
 
 	if((vocabData = LoadFile(FALSE, vocabName, NULL))==NULL)
     	return FALSE;
-	if((tokData = LoadFile(TRUE, "words.tok", NULL))==NULL)
+	if((tokData = LoadFile(TRUE, _T("words.tok"), NULL))==NULL)
     	return FALSE;
 
     wordData = (U8*)malloc(64000);
@@ -795,7 +796,7 @@ void ExecuteIF()
                         	n=*m;
                         	if(n<0||n>255) {
                             	m=m;
-                            	ErrorMessage("M");
+                            	ErrorMessage(_T("M"));
                             }
                             if(!lwPtrs[n])
                             	lwPtrs[n] = (U8*)calloc(lwfsize,1);
@@ -1089,14 +1090,14 @@ void FreeGame()
 }
 
 /******************************************************************************/
-VERLIST *FindAGIVersion(char *filename)
+VERLIST *FindAGIVersion(TCHAR *filename)
 {
 	FILE *f;
 	long len;
 	U8 *buffer,*p,*pend;
 	int major=-1,minor;
 
-	if((f=fopen(filename,"rb"))==NULL)
+	if((f=_tfopen(filename,_T("rb")))==NULL)
 		return NULL;
 
 	len = ftell(f);

romgui/addgame.cpp
@@ -56,7 +56,7 @@ void TFormAddGame::UpdateControls()
 	tbPath->Update();
 }
 //---------------------------------------------------------------------------
-void TFormAddGame::SetUp(AnsiString path)
+void TFormAddGame::SetUp(VclString path)
 {
 	tbPath->Text = path;
 
@@ -79,19 +79,19 @@ void TFormAddGame::SetUp(AnsiString path)
 	dropVersion->ItemIndex	= 1; // version 2 common
 	tbId->Enabled = false;
 
-	char *s = GetPathFolderName();
+	TCHAR *s = GetPathFolderName();
 	tbName->Text = s;
 	delete s;
 
     UpdateControls();
 }   
 //---------------------------------------------------------------------------
-char *TFormAddGame::GetPathString()
+TCHAR *TFormAddGame::GetPathString()
 {
-	char *pathstr = new char[tbPath->Text.Length()+tbId->Text.Length()+32];
+	TCHAR *pathstr = new TCHAR[tbPath->Text.Length()+tbId->Text.Length()+32];
 	int pathlen = tbPath->Text.Length();
 
-    strcpy(pathstr, tbPath->Text.c_str());
+    _tcscpy(pathstr, tbPath->Text.c_str());
 
 	if(pathlen && pathstr[pathlen-1] == '\\')
 		pathlen--;
@@ -100,21 +100,21 @@ char *TFormAddGame::GetPathString()
 	return pathstr;
 }
 //---------------------------------------------------------------------------
-char *TFormAddGame::GetPathFolderName()
+TCHAR *TFormAddGame::GetPathFolderName()
 {
-	char *pathstr;
+	TCHAR *pathstr;
 	int pathlen,i;
 
 	pathstr = GetPathString();
-	pathlen = strlen(pathstr);
+	pathlen = _tcslen(pathstr);
 
 	for(i=pathlen-1; i >= 0; i--) {
 		if(pathstr[i] == '\\') {
-			strcpy(pathstr,pathstr+i+1);
+			_tcscpy(pathstr,pathstr+i+1);
 			break;
 		}
 	}
-	if(strlen(pathstr)>25)
+	if(_tcslen(pathstr)>25)
 		pathstr[25]='\0';
 	return pathstr;
 }
@@ -127,9 +127,9 @@ void __fastcall TFormAddGame::btnCancelClick(TObject *Sender)
 void __fastcall TFormAddGame::btnOKClick(TObject *Sender)
 {
 	gameinfo.version	= &verlist[dropVersion->ItemIndex];
-	gameinfo.vID		= strdup((dropVersion->ItemIndex >= 3 )? tbId->Text.c_str():"") ;
-	gameinfo.title		= strdup(tbName->Text.c_str());
-	gameinfo.path		= strdup((tbPath->Text+"\\").c_str());
+	gameinfo.vID		= strdup_tchar_to_char((dropVersion->ItemIndex >= 3 )? tbId->Text.c_str():_T("")) ;
+	gameinfo.title		= strdup_tchar_to_char(tbName->Text.c_str());
+	gameinfo.path		= _tcsdup((tbPath->Text+_T("\\")).c_str());
 
 	okClose = TRUE;
 	Close();
@@ -142,27 +142,27 @@ void __fastcall TFormAddGame::dropVersionChange(TObject *Sender)
 //---------------------------------------------------------------------------
 void __fastcall TFormAddGame::btnAutodetectClick(TObject *Sender)
 {
-	char *pathstr;
+	TCHAR *pathstr;
 
 	pathstr = GetPathString();
-	strcat(pathstr,"\\agidata.ovl");
+	_tcscat(pathstr,_T("\\agidata.ovl"));
 
 	if(!FileExists(pathstr))
-		ShowMessage("Could not find file: "+AnsiString(pathstr));
+		ShowMessage(_T("Could not find file: ")+VclString(pathstr));
 	else {
 		VERLIST *version = FindAGIVersion(pathstr);
 		if(version) {
-			char s[128];
-			sprintf(s,"Found Version: %s, %d.%04X",version->name,version->ver.major,version->ver.minor);
+			TCHAR s[128];
+			_stprintf_s(s,_countof(s),_T("Found Version: %s, %d.%04X"),version->name,version->ver.major,version->ver.minor);
 			ShowMessage(
 					s,
-					"Version found!"
+					_T("Version found!")
 				);
 			dropVersion->ItemIndex = version - verlist;
 		} else {
 			ShowMessage(
-					"Unable to find version",
-					"Version not found!"
+					_T("Unable to find version"),
+					_T("Version not found!")
 				);
 		}
 	}
@@ -173,7 +173,7 @@ void __fastcall TFormAddGame::btnAutodetectClick(TObject *Sender)
 void __fastcall TFormAddGame::dropGamesChange(TObject *Sender)
 {
 	if(dropGames->ItemIndex != -1) {
-		if(dropGames->Text != "<Other>") {
+		if(dropGames->Text != _T("<Other>")) {
 			GAMEINFO *gi 			= games+dropGames->ItemIndex;
 			tbName->Text			= gi->title;
 			tbId->Text				= gi->vID;

romgui/addgame.h
@@ -61,10 +61,10 @@ private:	// User declarations
 public:		// User declarations
 	__fastcall TFormAddGame(TComponent* Owner);
 
-    void SetUp(AnsiString path);
+    void SetUp(VclString path);
     
-	char *GetPathString(void);
-	char *GetPathFolderName(void);
+	TCHAR *GetPathString(void);
+	TCHAR *GetPathFolderName(void);
 
     void UpdateControls();
 

romgui/addgame.rc
@@ -21,7 +21,7 @@ FONT 8, "Verdana", 400, 0
  CONTROL "", IDC_DROPGAMES, "combobox", WS_CHILD | WS_VSCROLL | CBS_HASSTRINGS | CBS_AUTOHSCROLL | CBS_DROPDOWNLIST | WS_VISIBLE, 4 , 9, 230 , 76, 0x00
  CONTROL "", IDC_DROPVERSION, "combobox", WS_CHILD | WS_VSCROLL | CBS_HASSTRINGS | CBS_AUTOHSCROLL | CBS_DROPDOWNLIST | WS_VISIBLE, 36 , 79, 139 , 76, 0x00
  CONTROL "", IDC_PANEL11, "static", WS_CHILD | NOT WS_VISIBLE | WS_TABSTOP, 157 , 97, 80 , 15, 0x00
- CONTROL "", IDC_TBPATH, "edit", WS_CHILD | WS_VISIBLE | ES_READONLY | ES_NOHIDESEL | ES_OEMCONVERT, 36 , 24, 198 , 12, 0x00 | WS_EX_CLIENTEDGE
+ CONTROL "", IDC_TBPATH, "edit", WS_CHILD | WS_VISIBLE | ES_READONLY | ES_NOHIDESEL, 36 , 24, 198 , 12, 0x00 | WS_EX_CLIENTEDGE
  CONTROL "", IDC_TBNAME, "edit", WS_CHILD | WS_VISIBLE | ES_NOHIDESEL | ES_OEMCONVERT, 36 , 49, 198 , 12, 0x00 | WS_EX_CLIENTEDGE
  CONTROL "", IDC_TBID, "edit", WS_CHILD | WS_VISIBLE | ES_NOHIDESEL | ES_OEMCONVERT, 36 , 64, 22 , 12, 0x00 | WS_EX_CLIENTEDGE
  CONTROL "Auto Detect", IDC_BTNAUTODETECT, "button", WS_CHILD | BS_PUSHBUTTON | WS_VISIBLE | WS_TABSTOP, 175 , 77, 59 , 15, 0x00

romgui/decompress.cpp
@@ -84,7 +84,7 @@ U8 *decode_string(U8 *buffer, U32 code)
 		code=prefix_code[code];
 		if (i++>=4000)
 		{
-			ErrorMessage("lzw: error in code expansion.\n");
+			ErrorMessage(_T("lzw: error in code expansion.\n"));
             return NULL;
 		}
 	}

romgui/dirdialog.cpp
@@ -33,7 +33,7 @@ TDirDialog *ddPtr;
 //---------------------------------------------------------------------------
 int __stdcall BrowseProc(HWND hwnd,UINT uMsg, LPARAM lParam, LPARAM lpData )
 {
-    char szDir[MAX_PATH];
+    TCHAR szDir[MAX_PATH];
 
     switch(uMsg)
     {
@@ -41,7 +41,7 @@ int __stdcall BrowseProc(HWND hwnd,UINT uMsg, LPARAM lParam, LPARAM lpData )
             // Set the initial directory. If WPARAM is TRUE, then LPARAM is a
             // string that contains the path. If WPARAM is FALSE, then LPARAM
             // should be a lovely PIDL
-            if(ddPtr->InitialDir!="") SendMessage(hwnd, BFFM_SETSELECTION, TRUE, (LPARAM)ddPtr->InitialDir.c_str());
+            if(ddPtr->InitialDir!=_T("")) SendMessage(hwnd, BFFM_SETSELECTION, TRUE, (LPARAM)ddPtr->InitialDir.c_str());
             SetWindowText(hwnd,ddPtr->Title.c_str());
             ddPtr->Handle = hwnd;
             
@@ -57,13 +57,13 @@ int __stdcall BrowseProc(HWND hwnd,UINT uMsg, LPARAM lParam, LPARAM lpData )
 //---------------------------------------------------------------------------
 TDirDialog::TDirDialog()
 {
-    AnsiString Title = "Select a folder.";
-    InitialDir = "";
+    VclString Title = _T("Select a folder.");
+    InitialDir = _T("");
     MAP_CHECK = FALSE;
 }       
 //---------------------------------------------------------------------------
-char DirectoryString[2048];
-void TDirDialog::CheckMap(const char *szDir)
+TCHAR DirectoryString[2048];
+void TDirDialog::CheckMap(const TCHAR *szDir)
 {
     BOOL FILE_FOUND;
     WIN32_FIND_DATA FindFileData;
@@ -71,15 +71,15 @@ void TDirDialog::CheckMap(const char *szDir)
 	if(!MAP_CHECK) return;
 
     if(szDir[3])
-        wsprintf(DirectoryString, "%s\\words.tok", szDir);
+        wsprintf(DirectoryString, _T("%s\\words.tok"), szDir);
     else
-        wsprintf(DirectoryString, "%swords.tok", szDir);
+        wsprintf(DirectoryString, _T("%swords.tok"), szDir);
 
     FILE_FOUND = FindFirstFile((LPCTSTR)DirectoryString,(LPWIN32_FIND_DATA)&FindFileData)!=INVALID_HANDLE_VALUE;
  	if(!FILE_FOUND) {
-    	SendMessage(Handle, BFFM_SETSTATUSTEXT, FALSE, (LPARAM)"AGI Game exists: NO");
+    	SendMessage(Handle, BFFM_SETSTATUSTEXT, FALSE, (LPARAM)_T("AGI Game exists: NO"));
    	} else {
-    	SendMessage(Handle, BFFM_SETSTATUSTEXT, FALSE, (LPARAM)"AGI Game exists: YES");
+    	SendMessage(Handle, BFFM_SETSTATUSTEXT, FALSE, (LPARAM)_T("AGI Game exists: YES"));
     }
     SendMessage(Handle, BFFM_ENABLEOK, 0, LPARAM(FILE_FOUND));
 }
@@ -87,8 +87,8 @@ void TDirDialog::CheckMap(const char *szDir)
 BOOL TDirDialog::Execute()
 {
     BROWSEINFO    info;
-    char          szDir[MAX_PATH];
-    char          szDisplayName[MAX_PATH];
+    TCHAR         szDir[MAX_PATH];
+    TCHAR         szDisplayName[MAX_PATH];
     LPITEMIDLIST  pidl;
     LPMALLOC      pShellMalloc;
 
@@ -112,10 +112,10 @@ BOOL TDirDialog::Execute()
         {
             if(SHGetPathFromIDList(pidl, szDir))
             {
-            	int sl=strlen(szDir);
+            	int sl=_tcsclen(szDir);
     			if(szDir[sl-1]=='\\')
              		szDir[sl-1]='\0';
-                FullPath = AnsiString(szDir);
+                FullPath = VclString(szDir);
             }
 
             pShellMalloc->Free(pidl);

romgui/dirdialog.h
@@ -28,9 +28,9 @@ public:
 	TDirDialog();
 	BOOL Execute();
     HWND Handle;
-    AnsiString Title,Caption,InitialDir,FullPath;
+    VclString Title,Caption,InitialDir,FullPath;
     BOOL MAP_CHECK;
-    void CheckMap(const char *szDir);
+    void CheckMap(const TCHAR *szDir);
 };
 #endif
  

romgui/main.cpp
@@ -35,17 +35,17 @@ __fastcall TFormMain::TFormMain(TComponent* Owner)
 {
     CreateControls();
 
-    strcpy(szPath,GetCurrentDir().c_str());
-    if(!FileExists(ProgramDir+"\\gbinjectb.exe"))
+    _tcscpy(szPath,GetCurrentDir().c_str());
+    if(!FileExists(ProgramDir+_T("\\gbinjectb.exe")))
     	GetProgramPath();
-    int l=strlen(szPath);
+    int l=_tcslen(szPath);
     if(szPath[l-1]=='\\')
     	szPath[l-1]='\0'; 
-    ProgramDir=AnsiString(GetProgramPath());
+    ProgramDir=VclString(GetProgramPath());
 
-    tbInput->Text	= ProgramDir+"\\gbagi.bin";
-    tbVocab->Text	= ProgramDir+"\\vocab.bin";
-    tbOutput->Text	= ProgramDir+"\\agigames.gba";
+    tbInput->Text	= ProgramDir+_T("\\gbagi.bin");
+    tbVocab->Text	= ProgramDir+_T("\\vocab.bin");
+    tbOutput->Text	= ProgramDir+_T("\\agigames.gba");
 
 	DirDialog = new TDirDialog;
 
@@ -65,7 +65,7 @@ void __fastcall TFormMain::FormDestroy(TObject *Sender)
 
 void __fastcall TFormMain::UpdateControls()
 {
-	btnBuild->Enabled 	= (tbOutput->Text!="" && listbox->Items->Count > 0);
+	btnBuild->Enabled 	= (tbOutput->Text!=_T("") && listbox->Items->Count > 0);
 	btnRemove->Enabled 	= (listbox->Items->Count > 0);
 
 	// Generated;
@@ -90,13 +90,13 @@ void __fastcall TFormMain::FormResize(TObject *Sender)
 void __fastcall TFormMain::btnAddClick(TObject *Sender)
 {
 	if(listbox->Items->Count >= 16) {
-     	ShowMessage("Maximum games added to game list");
+     	ShowMessage(_T("Maximum games added to game list"));
         return;
     }
 
 	// Get the directory which the game is located
-    DirDialog->Title = "Add Game";
-    DirDialog->Caption = "Please select the directory which the game you wish to add is located in.";
+    DirDialog->Title = _T("Add Game");
+    DirDialog->Caption = _T("Please select the directory which the game you wish to add is located in.");
     DirDialog->MAP_CHECK = TRUE;
     if(!DirDialog->Execute()) {
         return;
@@ -209,22 +209,22 @@ BOOL TFormMain::PackGames()
 
 	int totalGames = listbox->Items->Count;
 
-	inromName	= _strdup(tbInput->Text.c_str());
-	outromName	= _strdup(tbOutput->Text.c_str());
-	vocabName	= _strdup(tbVocab->Text.c_str());
+	inromName	= _tcsdup(tbInput->Text.c_str());
+	outromName	= _tcsdup(tbOutput->Text.c_str());
+	vocabName	= _tcsdup(tbVocab->Text.c_str());
 
-	if(FileExists(vocabName) == false || (fin=fopen(vocabName,"rb"))==NULL) {
- 		ShowMessage("Error opening vocab definition file! Please specify the location of the file included with this program by clicking on the \"Browse...\" button.");
+	if(FileExists(vocabName) == false || (fin=_tfopen(vocabName,_T("rb")))==NULL) {
+ 		ShowMessage(_T("Error opening vocab definition file! Please specify the location of the file included with this program by clicking on the \"Browse...\" button."));
 		return FALSE;
 	}
 	fclose(fin);
-	if(FileExists(inromName) == false || (fin=fopen(inromName,"rb"))==NULL) {
- 		ShowMessage("Error opening input rom! Please specify the location of the file included with this program by clicking on the \"Browse...\" button.");
+	if(FileExists(inromName) == false || (fin=_tfopen(inromName,_T("rb")))==NULL) {
+ 		ShowMessage(_T("Error opening input rom! Please specify the location of the file included with this program by clicking on the \"Browse...\" button."));
 		return FALSE;
 	}
-	if((fout=fopen(outromName,"wb"))==NULL) {
+	if((fout=_tfopen(outromName,_T("wb")))==NULL) {
     	fclose(fin);
- 		ShowMessage("Error opening file for output!");
+ 		ShowMessage(_T("Error opening file for output!"));
 		return FALSE;
 	}
 
@@ -262,11 +262,11 @@ BOOL TFormMain::PackGames()
 		ginm.title = gameobj->gameinfo.title;
 		ginm.version = gameobj->gameinfo.version;
 		ginm.vID = gameobj->gameinfo.vID;
-		txStatus->Caption = "Adding Game: "+AnsiString(gameobj->gameinfo.title);
+		txStatus->Caption = _T("Adding Game: ")+VclString(gameobj->gameinfo.title);
 		if(!ProcessGame(&ginm)) {
     		FreeGame();
     		fclose(fout);
-			ShowMessage("Error adding game: "+AnsiString(gameobj->gameinfo.title));
+			ShowMessage(_T("Error adding game: ")+VclString(gameobj->gameinfo.title));
 			return FALSE;
 		}
     	FreeGame();
@@ -282,21 +282,21 @@ BOOL TFormMain::PackGames()
 //---------------------------------------------------------------------------
 void __fastcall TFormMain::btnBuildClick(TObject *Sender)
 {
-	if(tbOutput->Text == "" || tbInput->Text == "") {
-		ShowMessage("You must specify an input and output file name!");
+	if(tbOutput->Text == _T("") || tbInput->Text == _T("")) {
+		ShowMessage(_T("You must specify an input and output file name!"));
 	} else {
 	if(listbox->Items->Count == 0) {
-		ShowMessage("You must add games to embed in the ROM!");
+		ShowMessage(_T("You must add games to embed in the ROM!"));
 	} else {
 		Enabled = false;
 		if(PackGames()) {
-			ShowMessage("ROM Build finished. Enjoy!");
+			ShowMessage(_T("ROM Build finished. Enjoy!"));
 
-            tbOutput->Text = "";
+            tbOutput->Text = _T("");
             RemoveAddGames();
     		UpdateControls();
 		}
-		txStatus->Caption = "";
+		txStatus->Caption = _T("");
 		delete inromName;
 		delete outromName;
 		delete vocabName;
@@ -323,10 +323,10 @@ LPCTSTR TFormMain::GetProgramPath()
 	BOOL bInString = FALSE;
 	static TCHAR szPath[4096];
 
-	strcpy(szPath, GetCommandLine());
+	_tcscpy(szPath, GetCommandLine());
 
 	// Extract first argument
-	for (i = 0; i < (int)strlen(szPath); i++)
+	for (i = 0; i < (int)_tcslen(szPath); i++)
 	{
 		if (szPath[i] == '"')
 			bInString = !bInString;
@@ -341,7 +341,7 @@ LPCTSTR TFormMain::GetProgramPath()
 	// Remove file name
 	bool bFoundPath = FALSE;
 
-	for (i = strlen(szPath) - 1; i >= 0; i--)
+	for (i = _tcslen(szPath) - 1; i >= 0; i--)
 		if (szPath[i] == '\\')
 		{
 			szPath[i] = 0;
@@ -370,7 +370,7 @@ void __fastcall TFormMain::FormShow(TObject *Sender)
 //---------------------------------------------------------------------------
 void __fastcall TFormMain::Label3Click(TObject *Sender)
 {
-	ShellExecute(NULL, NULL, "http://www.bripro.com", NULL, NULL, 0);
+	ShellExecute(NULL, NULL, _T("http://www.bripro.com"), NULL, NULL, 0);
 }
 //---------------------------------------------------------------------------
 

commit 0e2bf83dab61dc6d03f992ee1820f6ee7ea36a09

romgui: Fix Unicode support
For some reason this didn't affect the Release build, but it did prevent the Debug build from working properly.

Bug introduced in 41c73137cb54e1c00fc4d4d84d87644a92f05107

romgui/makerom.cpp
@@ -178,7 +178,7 @@ BOOL LoadDir(BOOL SINGLE, int num)
 	U32 offs;
 	int totalEnts, i;
 	if(SINGLE) {
-		_stprintf_s(fname,_countof(fname),_T("%s%s%s"),gi->path,gi->vID,gi->vID[0]?_T("dir"):_T("dirs"));
+		_stprintf_s(fname,_countof(fname),_T("%s%hs%s"),gi->path,gi->vID,gi->vID[0]?_T("dir"):_T("dirs"));
 		if((f=_tfopen(fname,_T("rb")))==NULL) {
 			ErrorMessage(_T("Unable to open file: %s, for reading!"),fname);
 			return FALSE;
@@ -224,7 +224,7 @@ BOOL PrepDirs()
 	volSize=0;
 	for(vn=0;vn<16;vn++) {
     	if(!VUSED[vn]) continue;
-		_stprintf_s(fname,_countof(fname),_T("%s%svol.%d"),gi->path,gi->vID,vn);
+		_stprintf_s(fname,_countof(fname),_T("%s%hsvol.%d"),gi->path,gi->vID,vn);
 		if((f=_tfopen(fname,_T("rb")))==NULL) {
         	VUSED[vn] = FALSE;
         	continue;
@@ -278,7 +278,7 @@ BOOL ProcessDirs()
 	for(vn=-1;vn<16;vn++) {
     	if(vn!=-1) {
         	if(!VUSED[vn]) continue;
-			_stprintf_s(fname,_countof(fname),_T("%s%svol.%d"),gi->path,gi->vID,vn);
+			_stprintf_s(fname,_countof(fname),_T("%s%hsvol.%d"),gi->path,gi->vID,vn);
 			if((f=_tfopen(fname,_T("rb")))==NULL) {
 				ErrorMessage(_T("Unable to open file: %s, for reading!"),fname);
 				mFree(fbuf);