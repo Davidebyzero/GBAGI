Davidebyzero/GBAGI

commit b621b47a634c0c8ea1db9eaef461e74319b7c1df

Allow vertical "scrolling" to show otherwise hidden parts of screen
The SELECT button has been rebound to step through three vertical screen positions (replacing its previous binding of showing the inventory, which can still be done from the menu system or by pressing TAB from the keyboard dialog).
This allows seeing the top 12 or bottom 4 pixel rows of picture, and bottom 3 rows of text, that were previously impossible to see in GBAGI.

Known Issues (will need fixing):
* Pressing SELECT erases text that has already been drawn.
* Sometimes, pressing SELECT will skip the intro.

cmdagi.c
@@ -2149,9 +2149,9 @@ void cSetPriBase()
 
 	PRI_FIXED = FALSE;
 
-	for(y=0; y<PIC_HEIGHT; y++) {
+	for(y=0; y<PRI_HEIGHT; y++) {
 		if((db = y-code[0]) >= 0) {
-			if((priTable[y] = (db*10)/(PIC_HEIGHT-code[0])+5) > 15)
+			if((priTable[y] = (db*10)/(PRI_HEIGHT-code[0])+5) > 15)
 				priTable[y] = 15;
 		} else
 			priTable[y] = 4;

input.c
@@ -26,6 +26,7 @@
 #include "text.h"
 #include "wingui.h"
 #include "keyboard.h"
+#include "screen.h" // for Y_ADJUST_CL and RedrawScreenAll()
 #ifdef _WINDOWS
 #include <windows.h>
 #endif
@@ -119,7 +120,15 @@ void PollInput()
                 		ExecuteKeyboardDialog();
                         break;
                 	case KEY_SELECT:
+#if 0
                     	key = '\t'; // inventory
+#else
+                        {{}} if (Y_ADJUST_CL == -8) Y_ADJUST_CL =  4;
+                        else if (Y_ADJUST_CL <=  4) Y_ADJUST_CL = 32;
+                        else                        Y_ADJUST_CL = -8;
+                        RedrawScreenAll();
+                        key = '\0';
+#endif
                     	break;
                 	case KEY_TABREV:
                     	key = KEY_F3<<8; // repeat word

menu.c
@@ -385,7 +385,7 @@ void DrawMenuBar()
 void ClearMenuItems()
 {
     if(menuRect.right) {
-    	RenderUpdate(0,0/*menuRect.top*/,PIC_MAXX,menuRect.bottom+menuRect.top);
+    	RenderUpdate(0,Y_ADJUST_CL-4/*menuRect.top*/,PIC_MAXX,menuRect.bottom+menuRect.top+Y_ADJUST_CL-4);
         menuRect.right=0;
     }
 }

screen.c
@@ -39,6 +39,7 @@
 #endif
 _RECT port;
 BOOL SHOW_VERSION;
+S16 Y_ADJUST_CL = 4;
 /*****************************************************************************/
 const int scrCoords[160][3] = {
 	{  0,  0,  0},{  0,  0,  0},{  0,  0,  0},{  0,  0,  0},{  3,  4,  1},{  3,  4,  1},{  3,  4,  1},{  3,  4,  1},
@@ -139,6 +140,13 @@ void ShakeScreen(int count)
         }
     }      
 	RedrawScreen();
+}
+/*****************************************************************************/
+void RedrawScreenAll()
+{
+    if(!STATUS_VISIBLE)
+        ClearLine(statusRow, clBLACK);
+    RedrawScreen();
 }
 /*****************************************************************************/
 void RedrawScreen()
@@ -336,16 +344,19 @@ void RenderUpdate(int x1, int y1, int x2, int y2)
 	U16 *s;
 	int w,h,maxY, yd,ye;
 
+	if(Y_ADJUST_CL>8 && y2>=PIC_MAXY)
+		RectFill(0,SCREEN_HEIGHT+8-Y_ADJUST_CL,SCREEN_WIDTH,SCREEN_HEIGHT,0);
+
 	if((x1>=PIC_WIDTH)||x2<0)
 		return;
 	if(x1<0) x1=0;
 
 	if(STATUS_VISIBLE) {
-		maxY = PIC_MAXY-16;
+		maxY = PIC_MAXY-Y_ADJUST_CL-8;
         yd = Y_ADJUST_CL+8;
         ye=8;
 	} else {
-		maxY = PIC_MAXY-8;
+		maxY = PIC_MAXY-Y_ADJUST_CL;
         yd = Y_ADJUST_CL;    
         ye=0;
 	}

screen.h
@@ -22,7 +22,8 @@
 #define _SCREEN_H
 /*****************************************************************************/
 //#define Y_ADJUST_ST			12//8//16
-#define Y_ADJUST_CL 		4//4//0//8
+//#define Y_ADJUST_CL 		32//-8//4//4//0//8
+extern S16 Y_ADJUST_CL;
 
 #define SCREEN_WIDTH		240
 #define SCREEN_HEIGHT		160
@@ -32,15 +33,17 @@
 
 #define PIC_WIDTH			160
 #define PIC_HEIGHT			168
-#define PIC_MAXX			159
-#define PIC_MAXY			167
-#define PIC_SIZE			26880
+#define PIC_MAXX			(PIC_WIDTH-1)
+#define PIC_MAXY			(PIC_HEIGHT-1)
+#define PIC_SIZE			(PIC_WIDTH*PIC_HEIGHT)
 
 #define PLAY_WIDTH			160
 #define PLAY_SIZE           32000
 
 #define PRI_WIDTH			80
-#define PRI_SIZE           	13440
+#define PRI_HEIGHT			PIC_HEIGHT
+#define PRI_MAXY  			(PRI_HEIGHT-1)
+#define PRI_SIZE           	(PRI_WIDTH*PRI_HEIGHT)
 
 #define GBA_MAXROW			19
 #define GBA_MAXCOL			39
@@ -64,6 +67,7 @@ extern BOOL SHOW_VERSION;
 /*****************************************************************************/
 void ShakeScreen(int count);
 void RotatePicBuf(void);
+void RedrawScreenAll(void);
 void RedrawScreen(void);
 void UpdateGfx(void);
 void DrawPlayArea(void);

views.c
@@ -206,8 +206,8 @@ void UpdateObjsStep()
 				if((v->y-v->height) < -1) {
 					v->y	= v->height - 1;
 					border	= bdTOP;
-				} else if(v->y > PIC_MAXY) {
-					v->y	= PIC_MAXY;
+				} else if(v->y > PRI_MAXY) {
+					v->y	= PRI_MAXY;
 					border	= bdBOTTOM;
 				} else if( (!(v->flags&oINGOREHORIZON)) && (horizon>=v->y) ) {
 					v->y	= horizon + 1;
@@ -996,7 +996,7 @@ void AddObjPicPri(VOBJ *v)
 // calculates the priority for view objects
 int CalcPriY(int pri)
 {
-	int priY = PIC_MAXY;
+	int priY = PRI_MAXY;
 
 	if(PRI_FIXED)
 		return((pri-5)*12 + 48);
@@ -1028,7 +1028,7 @@ void ShowObj(int num)
     objView.x 			=
     objView.prevX 		= (PIC_MAXX-objView.width)>>1;
     objView.y 			=
-    objView.prevY 		= (PIC_MAXY-10);
+    objView.prevY 		= (PRI_MAXY-10);
     objView.priority	= 15;
     objView.flags		|= oFIXEDPRIORITY;
     objView.num			= 255;

commit 49a1b268a2e9739bccef5a81d2e008ca0681a5a1

Remember drawn text and redraw it upon vertical "scrolling"
This fixes the main bug in the feature introduced in b621b47a634c0c8ea1db9eaef461e74319b7c1df.

cmdagi.c
@@ -387,12 +387,12 @@ void cOverlayPic()
 void cShowPriScreen()
 {
 	RotatePicBuf();
-	RenderUpdate(0,0,PIC_MAXX,PIC_MAXY);
+	RenderUpdate(0,0,PIC_MAXX,PIC_MAXY,TRUE);
 
 	WaitEnterEsc();
 
 	RotatePicBuf();
-	RenderUpdate(0,0,PIC_MAXX,PIC_MAXY);
+	RenderUpdate(0,0,PIC_MAXX,PIC_MAXY,TRUE);
 }
 /******************************************************************************/
 //load.view(VIEWNUM);
@@ -1392,7 +1392,7 @@ void cTextScreen()
 void cGraphics()
 {
 	TEXT_MODE	= FALSE;
-	RedrawScreen();
+	RedrawScreen(TRUE);
 #ifdef _WINDOWS
     SystemUpdate();
 #endif
@@ -2098,7 +2098,7 @@ void cDivV()
 void cCloseWindow()
 {
     WINDOW_OPEN = FALSE;
-    RenderUpdate(0,0,PIC_MAXX,PIC_MAXY);
+    RenderUpdate(0,0,PIC_MAXX,PIC_MAXY,TRUE);
 }
 /******************************************************************************/
 //	The set.simple() command sets the save.game mode to automatic by giving a

input.c
@@ -123,10 +123,12 @@ void PollInput()
 #if 0
                     	key = '\t'; // inventory
 #else
-                        {{}} if (Y_ADJUST_CL == -8) Y_ADJUST_CL =  4;
-                        else if (Y_ADJUST_CL <=  4) Y_ADJUST_CL = 32;
-                        else                        Y_ADJUST_CL = -8;
-                        RedrawScreenAll();
+                        if(!TEXT_MODE) {
+                            {{}} if (Y_ADJUST_CL == -8) Y_ADJUST_CL =  4;
+                            else if (Y_ADJUST_CL <=  4) Y_ADJUST_CL = 32;
+                            else                        Y_ADJUST_CL = -8;
+                            RedrawScreenAll();
+                        }
                         key = '\0';
 #endif
                     	break;

main.c
@@ -53,6 +53,8 @@ int main()
     for(;;) {
     	InitWinGUISystem(0);
 
+        ClearTextBuf();
+        EraseBottomText();
         LoadPicBufSplash();
 
         PIC_VISIBLE=TRUE;

menu.c
@@ -385,7 +385,7 @@ void DrawMenuBar()
 void ClearMenuItems()
 {
     if(menuRect.right) {
-    	RenderUpdate(0,Y_ADJUST_CL-4/*menuRect.top*/,PIC_MAXX,menuRect.bottom+menuRect.top+Y_ADJUST_CL-4);
+    	RenderUpdate(0,Y_ADJUST_CL-4/*menuRect.top*/,PIC_MAXX,menuRect.bottom+menuRect.top+Y_ADJUST_CL-4,FALSE);
         menuRect.right=0;
     }
 }

picture.c
@@ -99,6 +99,7 @@ void DrawPic(U8 num)
 	PIC_VISIBLE = FALSE;
 
 //cShowPriScreen();
+    EraseBottomText();
 }
 
 /*****************************************************************************/
@@ -498,7 +499,7 @@ int j;
 for(j=0;j<160*168;j++)
 	fputc(pictureBuf[j]&0xF0,f);
 fclose(f);
-RenderUpdate(0,0,PIC_MAXX,PIC_MAXY);
+RenderUpdate(0,0,PIC_MAXX,PIC_MAXY,TRUE);
 WaitEnterEsc();   */
 
     do

screen.c
@@ -33,9 +33,11 @@
 	U8 screenBuf[SCREEN_SIZE];
 	#define vidPtr ((U16*)screenBuf)  
 	U8 pictureBuf[PIC_SIZE];
+	U8 textBuf[TXT_WIDTH*TXT_HEIGHT*2];
 #else
 	U16 *vidPtr;
 	U8 pictureBuf[PIC_SIZE] _EWRAM_;
+	U8 textBuf[TXT_WIDTH*TXT_HEIGHT*2] _EWRAM_;
 #endif
 _RECT port;
 BOOL SHOW_VERSION;
@@ -139,20 +141,60 @@ void ShakeScreen(int count)
             }
         }
     }      
-	RedrawScreen();
+	RedrawScreen(TRUE);
 }
 /*****************************************************************************/
 void RedrawScreenAll()
 {
     if(!STATUS_VISIBLE)
         ClearLine(statusRow, clBLACK);
-    RedrawScreen();
+    RedrawScreen(FALSE);
 }
 /*****************************************************************************/
-void RedrawScreen()
+void RedrawScreen(BOOL clear)
 {
-	RenderUpdate(0,0,PIC_MAXX,PIC_MAXY);
+	RenderUpdate(0,0,PIC_MAXX,PIC_MAXY,clear);
 	WriteStatusLine();
+	if(Y_ADJUST_CL>8)
+		RectFill(0,SCREEN_HEIGHT+8-Y_ADJUST_CL,SCREEN_WIDTH,SCREEN_HEIGHT,0);
+    if(clear)
+        ClearTextBuf();
+    else {
+        U8 textRowOld = textRow;
+        U8 textColOld = textCol;
+        U8 textColourOld = textColour;
+        U8 *p = textBuf;
+        textRow = 0;
+        textCol = 0;
+        for(;;) {
+            extern int CalcTextY(int row,BOOL FIX);
+            if(p[0]) {
+                textColour = p[1];
+	            AbChar(p[0],
+    	            (textCol*CHAR_WIDTH),
+                    CalcTextY(textRow,TRUE),
+                    (p[1]&0xF),(p[1]>>4)
+                );
+            }
+            p += 2;
+            if(++textCol>=TXT_WIDTH) {
+                textCol = 0;
+                if(++textRow>=TXT_HEIGHT)
+                    break;
+            }
+        }
+    }
+}
+/*****************************************************************************/
+void ClearTextBuf()
+{
+    memset(textBuf,0,sizeof(textBuf));
+}
+/*****************************************************************************/
+void EraseBottomText()
+{
+	if(!TEXT_MODE && Y_ADJUST_CL>8)
+		RectFill(0,SCREEN_HEIGHT+8-Y_ADJUST_CL,SCREEN_WIDTH,SCREEN_HEIGHT,0);
 }
 /*****************************************************************************/
 void UpdateGfx()
@@ -297,7 +339,7 @@ void PicPlotPix(U8 x, U8 y, U8 c)
 void ShowPic()
 {
 	if(PIC_VISIBLE)
-    	RenderUpdate(0,0,PIC_MAXX,PIC_MAXY);
+    	RenderUpdate(0,0,PIC_MAXX,PIC_MAXY,TRUE);
     if(SHOW_VERSION) {
 		DrawStringAbs(11,132,"GBAGI v"BUILD_VERSION"",0xEF);
 		DrawStringAbs(9,141,"("BUILD_DATE")",0xEF);
@@ -337,15 +379,36 @@ void DrawScreenBlock(U8 *p,U16 *s,int w,int h)
     }
 }
 /*****************************************************************************/
-void RenderUpdate(int x1, int y1, int x2, int y2)
+void ClearTextInside(int x1, int y1, int x2, int y2)
+{
+    size_t len;
+    U8 *p;
+    x1 =  x1   /(PIC_WIDTH/TXT_WIDTH);
+    y1 =  y1   / CHAR_HEIGHT;
+    x2 = (x2-1)/(PIC_WIDTH/TXT_WIDTH)+2;
+    y2 = (y2-1)/ CHAR_HEIGHT         +2;
+    if (x2<x1)
+        x2=x1;
+    if (y2<y1)
+        y2=y1;
+    len = (x2-x1)*2;
+    p = &textBuf[y1*TXT_WIDTH*2 + x1*2];
+    for(;;) {
+        memset(p,0,len);
+        if(++y1 >= y2)
+            break;
+        p += TXT_WIDTH*2;
+    }
+}
+void RenderUpdate(int x1, int y1, int x2, int y2, BOOL clear)
 {
 
 	U8 *p;
 	U16 *s;
 	int w,h,maxY, yd,ye;
 
-	if(Y_ADJUST_CL>8 && y2>=PIC_MAXY)
-		RectFill(0,SCREEN_HEIGHT+8-Y_ADJUST_CL,SCREEN_WIDTH,SCREEN_HEIGHT,0);
+    if(clear)
+        ClearTextInside(x1,y1,x2,y2);
 
 	if((x1>=PIC_WIDTH)||x2<0)
 		return;
@@ -391,11 +454,52 @@ void ClearLine(int row, U8 c)
     }
 }
 /*****************************************************************************/
+void ClampTextX(int *x)
+{
+    if (*x<0)
+        *x=0;
+    else
+    if (*x>TXT_WIDTH-1)
+        *x=TXT_WIDTH-1;
+}
+void ClampTextY(int *y)
+{
+    if (*y<0)
+        *y=0;
+    else
+    if (*y>TXT_HEIGHT-1)
+        *y=TXT_HEIGHT-1;
+}
+/*****************************************************************************/
 void ClearTextRect(int x1, int y1, int x2, int y2, U8 c)
 {
 	int size,w;
 	U16 wC;
     U16 *p;
+    ClampTextX(&x1); ClampTextY(&y1);
+    ClampTextX(&x2); ClampTextY(&y2);
+    if(x2>=x1 && y2>=y1) {
+        int y = y1;
+        int len = x2-x1+1;
+        U16 *p = (U16*)&textBuf[y*TXT_WIDTH*2 + x1*2];
+        if(c==0) {
+            for(;;) {
+                memset(p,0,len*2);
+                if(++y > y2)
+                    break;
+                p += TXT_WIDTH;
+            }
+        } else {
+            wC = ' ' + (c << (4+8));
+            for(;;) {
+                for(w=0; w<len; w++)
+                    p[w] = wC;
+                if(++y > y2)
+                    break;
+                p += TXT_WIDTH;
+            }
+        }
+    }
     y1 = ((y1-1)*CHAR_HEIGHT)-Y_ADJUST_CL;
     y2 = ((y2)*CHAR_HEIGHT)-Y_ADJUST_CL;
     if(y2<0||y1>=SCREEN_HEIGHT-Y_ADJUST_CL) return;

screen.h
@@ -45,8 +45,11 @@ extern S16 Y_ADJUST_CL;
 #define PRI_MAXY  			(PRI_HEIGHT-1)
 #define PRI_SIZE           	(PRI_WIDTH*PRI_HEIGHT)
 
-#define GBA_MAXROW			19
-#define GBA_MAXCOL			39
+#define TXT_WIDTH           40
+#define TXT_HEIGHT          25
+
+#define GBA_MAXROW			(SCREEN_HEIGHT/8-1)
+#define GBA_MAXCOL			(TXT_WIDTH-1)
 #define GBA_MAXY			151
 #define GBASCR_SIZEOF		(19200)
 
@@ -56,10 +59,12 @@ extern S16 Y_ADJUST_CL;
 #ifdef _WINDOWS
 	extern U8 screenBuf[SCREEN_SIZE];   
 	extern U8 pictureBuf[PIC_SIZE];
+	extern U8 textBuf[TXT_WIDTH*TXT_HEIGHT*2];
 #else
 	//extern U8 *pictureBuf;		// the active vis/pri buffer (160x168x(4bit+4bit))--1 byte = (pri/vis)
    	extern U16 *vidPtr;   
 	extern U8 pictureBuf[PIC_SIZE] _EWRAM_;
+	extern U8 textBuf[TXT_WIDTH*TXT_HEIGHT*2] _EWRAM_;
 #endif
 
 extern _RECT port;
@@ -68,7 +73,9 @@ extern BOOL SHOW_VERSION;
 void ShakeScreen(int count);
 void RotatePicBuf(void);
 void RedrawScreenAll(void);
-void RedrawScreen(void);
+void RedrawScreen(BOOL clear);
+void ClearTextBuf();
+void EraseBottomText();
 void UpdateGfx(void);
 void DrawPlayArea(void);
 void ShowPic(void);
@@ -84,7 +91,7 @@ void ViewPlotPix(U16 x, U16 y, U8 cr);
 void PicPlotPix(U8 x, U8 y, U8 c);
 void ClearLine(int row, U8 c);
 void ClearTextRect(int x1, int y1, int x2, int y2, U8 c);
-void RenderUpdate(int x1, int y1, int x2, int y2);
+void RenderUpdate(int x1, int y1, int x2, int y2, BOOL clear);
 void ClearScreen(U8 c);
 void TransBox(int x1, int y1, int x2, int y2);
 void ShadowBox(int x1, int y1, int x2, int y2);

system.c
@@ -463,7 +463,7 @@ void gfxGUIExit()
     else
         vidPtr = ((U16*)0x6000000);
 #endif
-	RedrawScreen();
+	RedrawScreen(TEXT_MODE);
 #ifdef _WINDOWS
 	SystemUpdate();
 #endif

text.c
@@ -339,6 +339,11 @@ void DrawString(char *s,BOOL FIX)
         	textRow++;
             if(!--l||*++s=='\0') break;
         }
+        if(textCol<TXT_WIDTH && textRow<TXT_HEIGHT) {
+            U8 *p = &textBuf[textRow*TXT_WIDTH*2 + textCol*2];
+            p[0] = *s;
+            p[1] = textColour;
+        }
     	DrawChar(*s++,FIX);
         l--;
     }

views.c
@@ -602,7 +602,7 @@ void UpdateObjCel(VOBJ *v)
 	if(height-y1 > 0)
 		height = y1+1;
 
-	RenderUpdate(x1, y1-height+1, x1+width+1, y1+1);
+	RenderUpdate(x1, y1-height+1, x1+width+1, y1+1, TRUE);
 }
 /*****************************************************************************/
 void UpdateObjLoop(VOBJ *v)